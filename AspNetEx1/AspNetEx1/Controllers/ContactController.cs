6609d96a9fcf64c3e4832628b99aac0415e39054
Thomas Prunier <thomas.prunier@labom2iformation.fr>
2023-09-01 16:51:55 +0200

    Ex3

--- AspNetEx1/AspNetEx1/Controllers/ContactController.cs
+++ AspNetEx1/AspNetEx1/Controllers/ContactController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetEx1.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetEx1.Controllers
 {
     public class ContactController : Controller
     {
+
         public IActionResult Show()
         {
-            return View();
+            Contact contact = new Contact()
+            {
+                Id = 1,
+                FName = "John",
+                LName = "Doe"
+            };
+            return View(contact);
         }
 
         public IActionResult Add()
@@ -16,6 +24,20 @@ namespace AspNetEx1.Controllers
 
         public IActionResult Index()
         {
+            Contact contact1 = new Contact()
+            {
+                Id = 1,
+                FName = "John",
+                LName = "Doe"
+            };           
+            Contact contact2 = new Contact()
+            {
+                Id = 2,
+                FName = "Jane",
+                LName = "Doe"
+            };
+            List<Contact> contacts = new List<Contact>(new Contact[] {contact1, contact2 });
+            ViewBag.Contacts = contacts;
             return View();
         }
     }
---
+++ AspNetEx1/AspNetEx1/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetEx1.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+    }
+}
