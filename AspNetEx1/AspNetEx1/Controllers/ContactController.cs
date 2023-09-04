05c813efcab6c1e4e7873b9f15034b4fd5cd337b
Thomas Prunier <thomas.prunier@labom2iformation.fr>
2023-09-04 13:30:38 +0200

    Working form

--- AspNetEx1/AspNetEx1/Controllers/ContactController.cs
+++ AspNetEx1/AspNetEx1/Controllers/ContactController.cs
@@ -1,28 +1,34 @@
-﻿using AspNetEx1.Models;
+﻿using AspNetEx1.Data;
+using AspNetEx1.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+using System.Xml.Linq;
 
 namespace AspNetEx1.Controllers
 {
     public class ContactController : Controller
     {
-
-        List<Contact> fakeContacts = new List<Contact>(new Contact[] { new Contact() { Id = 0, FName = "Joe", LName = "Doe" }, new Contact() { Id = 1, FName = "Jane", LName = "Doe" } });
-
         public IActionResult Show(int id = 1)
         {
-            Contact contact = fakeContacts[id];
+            Contact contact = FakeContactDb.Get(id);
             return View(contact);
         }
 
-        public IActionResult Add()
+        public IActionResult Add([FromForm] string? fname, [FromForm] string? lname, [FromForm] string? email, [FromForm] string? phone)
         {
+            if(fname is not null && lname is not null)
+            {
+                FakeContactDb.Add(new Contact() { Id = FakeContactDb.Count, FName = fname, LName = lname, Email = email, Phone = phone });
+                ViewData["Status"] = 201;
+                ViewData["RouteId"] = FakeContactDb.Count-1;
+            }
             return View();
         }
 
         public IActionResult Index()
         {
 
-            ViewBag.Contacts = fakeContacts;
+            ViewBag.Contacts = FakeContactDb.Contacts;
             return View();
         }
     }
---
+++ AspNetEx1/AspNetEx1/Data/FakeContactDb.cs
@@ -0,0 +1,21 @@
+﻿using AspNetEx1.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AspNetEx1.Data
+{
+    public static class FakeContactDb
+    {
+        static public List<Contact> Contacts { get; } = new List<Contact>(new Contact[] { new Contact() { Id = 0, FName = "John", LName = "Doe" }, new Contact() { Id = 1, FName = "Jane", LName = "Doe" } });
+        static public int Count { get => Contacts.Count; }
+
+        public static Contact Get(int id)
+        {
+            return Contacts[id];
+        }
+
+        public static void Add(Contact c)
+        {
+            Contacts.Add(c);
+        }
+    }
+}
