93777f0e990f96a2a15def7d37938a99f87ee704
Thomas Prunier <thomas.prunier@labom2iformation.fr>
2023-09-04 09:27:55 +0200

    Refined contact details page

--- AspNetEx1/AspNetEx1/Controllers/ContactController.cs
+++ AspNetEx1/AspNetEx1/Controllers/ContactController.cs
@@ -6,14 +6,11 @@ namespace AspNetEx1.Controllers
     public class ContactController : Controller
     {
 
-        public IActionResult Show()
+        List<Contact> fakeContacts = new List<Contact>(new Contact[] { new Contact() { Id = 0, FName = "Joe", LName = "Doe" }, new Contact() { Id = 1, FName = "Jane", LName = "Doe" } });
+
+        public IActionResult Show(int id = 1)
         {
-            Contact contact = new Contact()
-            {
-                Id = 1,
-                FName = "John",
-                LName = "Doe"
-            };
+            Contact contact = fakeContacts[id];
             return View(contact);
         }
 
@@ -24,20 +21,8 @@ namespace AspNetEx1.Controllers
 
         public IActionResult Index()
         {
-            Contact contact1 = new Contact()
-            {
-                Id = 1,
-                FName = "John",
-                LName = "Doe"
-            };           
-            Contact contact2 = new Contact()
-            {
-                Id = 2,
-                FName = "Jane",
-                LName = "Doe"
-            };
-            List<Contact> contacts = new List<Contact>(new Contact[] {contact1, contact2 });
-            ViewBag.Contacts = contacts;
+
+            ViewBag.Contacts = fakeContacts;
             return View();
         }
     }
